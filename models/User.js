1f5df84e840ba8781376b80057e126c686af504f
Namrata4556 <rasalsavita611@gmail.com>
2026-07-12 16:58:05 +0530

    Create User.js

---
+++ models/User.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+
+    name: {
+        type: String,
+        required: true
+    },
+
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    password: {
+        type: String,
+        required: true
+    },
+
+    role: {
+        type: String,
+        enum: ["Admin", "Manager", "Driver"],
+        default: "Driver"
+    }
+
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
